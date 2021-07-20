d04ee1a87bfc62ce9cbc594011d65f3be59a3c76
samarkalra <skalra@ex2india.com>
2021-07-20 16:28:17 +0530

    added global colors and styles

---
+++ src/core/constants/appStyles.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from './colors';
+
+export const appStyles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: Colors.white,
+  },
+  container: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
---
+++ src/core/constants/colors.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+  white: '#FFFFFF',
+  black: '@000000',
+  red: '#FF0000',
+  blue: '#0000FF',
+  green: '#00FF00',
+};
