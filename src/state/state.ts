721943de64bb1ec0fb9e952c6b84e01765b92a98
samarkalra <skalra@ex2india.com>
2021-07-20 22:49:14 +0530

    added root reducer in store

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,10 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import counterReducer from './reducers/counterSlice';
 
+/* ************ Reducers ************ */
+const rootreducer = combineReducers({
+  counter: counterReducer,
+});
+
+/* ************ Store ************ */
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: rootreducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
