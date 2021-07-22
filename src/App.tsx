0535595a6db8e1463201530912d08af50932b263
samarkalra <skalra@ex2india.com>
2021-07-22 15:51:12 +0530

    implemented redux-persist to store counterSlice state in AsyncStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from './state/state';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistedStore, store} from './state/state';
 import {Counter} from './ui/screens/counter-screen';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Counter />
+      <PersistGate loading={null} persistor={persistedStore}>
+        <Counter />
+      </PersistGate>
     </Provider>
   );
 };
--- src/state/state.ts
+++ src/state/state.ts
@@ -1,15 +1,36 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 import counterReducer from './reducers/counterSlice';
+import {persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import persistStore from 'redux-persist/lib/persistStore';
 
 /* ************ Reducers ************ */
-const rootreducer = combineReducers({
-  counter: counterReducer,
+const counterReducerPersistConfig = {
+  key: 'counter',
+  storage: AsyncStorage,
+  whitelist: ['value'],
+};
+
+const rootReducer = combineReducers({
+  counter: persistReducer(counterReducerPersistConfig, counterReducer),
 });
 
 /* ************ Store ************ */
 export const store = configureStore({
-  reducer: rootreducer,
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: false,
+      immutableCheck: true,
+      serializableCheck: false,
+    }),
 });
+
+export const persistedStore = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
